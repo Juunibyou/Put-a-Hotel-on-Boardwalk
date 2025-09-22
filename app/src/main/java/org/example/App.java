e39c15949907a26e697d23b7ea8794693de60cf2
Juunibyou <179892743+Juunibyou@users.noreply.github.com>
2025-09-22 00:17:19 -0500

    update

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,11 +4,34 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+    CircularLinkedList monopolyBoard = new CircularLinkedList<string>();
+ 
+    monopolyBoard.append("Go");
+    monopolyBoard.append("Mediteranean Avenue");
+    monopolyBoard.append("Community Chest");
+    monopolyBoard.append("Baltic Avenue");
+    monopolyBoard.append("Income Tax");
+    monopolyBoard.append("Reading Railroad");
+    monopolyBoard.append("Oriental Avenue");
+    monopolyBoard.append("Chance");
+    monopolyBoard.append("Vermont Avenue");
+    monopolyBoard.append("Connecticut Avenue");
+ 
+    System.out.println(monopolyBoard.currentNode); // This should print "Go" to the console
+ 
+    monopolyBoard.step();
+ 
+    System.out.println(monopolyBoard.currentNode); // This should print "Mediteranean Avenue" to the console
+ 
+    monopolyBoard.step();
+    monopolyBoard.step();
+    monopolyBoard.step();
+ 
+    System.out.println(monopolyBoard.currentNode); // This should print "Income Tax" to the console
+ 
+    for(int i = 0; i < 37 i++) {
+        monopolyBoard.step();
     }
-
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+ 
+    System.out.println(monopolyBoard.currentNode); 
 }
